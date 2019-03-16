83ec300c6770d2e257979e92d75bb8a8140b0c11
Yohan Lee <Yohan@Yohan.local>
2019-03-15 20:26:21 -0400

    Diversio Connect boilerplate.

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import logo from './diversio.logo.png'
+import signout from './signout.svg';
+import './navbar.css'
+
+const NavBar = (props) => {
+    const path = window.location.pathname;
+
+    return (
+        <div className="NavBar" id="navbar">
+            <img src={ logo } className="navbar_logo" alt="Diversio Global"/>
+            <ul className="navbar_list">
+                <li className={ classNames({ selected: path === '/' }) }><Link to="/">Home</Link></li>
+                <li className={ classNames({ selected: path === '/analyze' }) }><Link to="/analyze">Analyze</Link></li>
+                <li className={ classNames({ selected: path === '/improve' }) }><Link to="/improve">Improve</Link></li>
+                <li className={ classNames({ selected: path === '/leverage' }) }><Link to="/leverage">Leverage</Link></li>
+                <li className={ classNames({ selected: path === '/connect' }) }><Link to="/connect">Connect</Link></li>
+                <li className={ classNames({ selected: path === '/learn-more' }) }><Link to="/learn-more">Learn More</Link></li>
+            </ul>
+            <div className="navbar_signout-container">
+                <img src={ signout } alt=""/>
+                <a href="" className="navbar_signout">Sign out</a>
+            </div>
+        </div>
+    )
+};
+
+export default NavBar;
